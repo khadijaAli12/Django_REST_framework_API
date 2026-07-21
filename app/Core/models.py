cf6a7c44c87f1f9405896ebede9d827d73b13884
Khadija Ali <Khadija.Ali@dha-tasdeeq.com>
2026-07-21 11:31:35 -0700

    session 13 : recipe apis build

--- app/Core/models.py
+++ app/Core/models.py
@@ -1,8 +1,8 @@
 """
 Database models.
 """
+from django.conf import settings
 from django.db import models
-
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
@@ -17,15 +17,26 @@ class UserManager(BaseUserManager):
         """Create, save and return a new user."""
         if not email:
             raise ValueError('User must have an email address.')
-        user = self.model(email=self.normalize_email(email), **extra_fields)
+
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, password):
+    def create_superuser(self, email, password, **extra_fields):
         """Create and return a new superuser."""
-        user = self.create_user(email, password)
+
+        user = self.create_user(
+            email,
+            password,
+            **extra_fields
+        )
+
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
@@ -35,6 +46,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -43,3 +55,21 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    """Recipe object."""
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
